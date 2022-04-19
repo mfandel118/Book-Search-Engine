f8df500100e356a365ee2786d1b5cc99c3d9e85d
Marissa Fandel <mfandel@alumni.nd.edu>
2022-04-19 15:25:37 -0600

    added User & Book typeDefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -3,7 +3,22 @@ const { gql } = require("apollo-server-express");
 
 // define schemas
 const typeDefs = gql`
-    type User {}`;
+	type User {
+		_id: ID!
+		username: String!
+		email: String!
+		bookCount: Int
+		savedBooks: [Book]
+	}
+	type Book {
+		bookId: ID!
+		authors: [String]
+		description: String!
+		title: String!
+		image: String
+		link: String
+	}
+`;
 
 // export typeDefs
 module.exports = typeDefs;

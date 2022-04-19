b259d6ac4ec5ab31441b532550cef87204a63d9e
Marissa Fandel <mfandel@alumni.nd.edu>
2022-04-18 19:40:39 -0600

    setup schemas/index.js

---
+++ server/schemas/index.js
@@ -0,0 +1,6 @@
+// import resolvers & typeDefs
+const typeDefs = require("./typeDefs");
+const resolvers = require("./resolvers");
+
+// export resolvers & typeDefs
+module.exports = { typeDefs, resolvers };

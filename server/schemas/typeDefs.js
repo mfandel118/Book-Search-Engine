3f6d9a0b36f221eea4cf1f60ae2db3374db5cf26
Marissa Fandel <mfandel@alumni.nd.edu>
2022-04-20 19:05:45 -0600

    updated typeDefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -18,19 +18,27 @@ const typeDefs = gql`
 		image: String
 		link: String
 	}
+	type SavedBooksInput {
+		bookId: ID!
+		authors: [String]
+		description: String
+		title: String!
+		image: String
+		link: String
+	}
 	type Auth {
 		token: ID!
 		user: User
 	}
-    type Query {
-        me: User
-    }
-    type Mutation {
-        login(email: String!, password: String!): Auth
-        addUser(username: String!, email: String!, password: String!): Auth
-        saveBook(): User
-        removeBook(bookId: ID!): User
-    }
+	type Query {
+		me: User
+	}
+	type Mutation {
+		login(email: String!, password: String!): Auth
+		addUser(username: String!, email: String!, password: String!): Auth
+		saveBook(book: SavedBooksInput): User
+		removeBook(bookId: ID!): User
+	}
 `;
 
 // export typeDefs

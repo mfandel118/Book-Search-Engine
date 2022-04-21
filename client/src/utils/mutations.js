acd0faa7d4f09937646fec639d528bea459d2a86
Marissa Fandel <mfandel@alumni.nd.edu>
2022-04-20 19:25:16 -0600

    started mutations.js on frontend

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,1 +1,25 @@
 import { gql } from "@apollo/client";
+
+export const LOGIN_USER = gql`
+    mutation login() {
+        login()
+    }
+`;
+
+export const ADD_USER = gql`
+    mutation addUser() {
+        addUser()
+    }
+`;
+
+export const SAVE_BOOK = gql`
+    mutation saveBook() {
+        saveBook()
+    }
+`;
+
+export const REMOVE_BOOK = gql`
+    mutation removeBook() {
+        removeBook()
+    }
+`;

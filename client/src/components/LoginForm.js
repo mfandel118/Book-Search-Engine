df60c331397780fb8d9c29d83d7967f20b8b9479
Marissa Fandel <mfandel@alumni.nd.edu>
2022-04-20 21:41:51 -0600

    refactor loginForm.js

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -13,6 +13,8 @@ const LoginForm = () => {
 	const [validated] = useState(false);
 	const [showAlert, setShowAlert] = useState(false);
 
+	const [login, { error, data }] = useMutation(LOGIN_USER);
+
 	const handleInputChange = (event) => {
 		const { name, value } = event.target;
 		setUserFormData({ ...userFormData, [name]: value });
@@ -29,15 +31,21 @@ const LoginForm = () => {
 		}
 
 		try {
-			const response = await loginUser(userFormData);
+			// const response = await loginUser(userFormData);
+			// if (!response.ok) {
+			// 	throw new Error("something went wrong!");
+			// }
+			// const { token, user } = await response.json();
+			// console.log(user);
+			// Auth.login(token);
 
-			if (!response.ok) {
-				throw new Error("something went wrong!");
-			}
+			const { data } = await login({
+				variables: { ...userFormData },
+			});
 
-			const { token, user } = await response.json();
-			console.log(user);
-			Auth.login(token);
+			// console.log(data);
+
+			Auth.login(data.login.token);
 		} catch (err) {
 			console.error(err);
 			setShowAlert(true);
